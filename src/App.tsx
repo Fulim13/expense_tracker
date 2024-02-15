b21795719e8137c57d914a9b7faa7eff45690a81
Fu Lim <fulim1130@gmail.com>
2024-02-16 01:12:49 +0800

    Refactoring Expense Form

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseFilter from "./components/ExpenseFilter";
 import ExpenseForm from "./components/ExpenseForm";
+import categories from "./categories";
 
 const initialExpenses = [
   { id: 1, title: "Milk", amount: 5, category: "Groceries" },
@@ -13,9 +14,9 @@ const initialExpenses = [
 function App() {
   const [expenses, setExpenses] = useState(initialExpenses);
   const [selectedCategory, setSelectedCategory] = useState("");
-  const [categories, setCatogories] = useState([
-    ...new Set(expenses.map((expense) => expense.category)),
-  ]);
+  // const [categories, setCatogories] = useState([
+  //   ...new Set(expenses.map((expense) => expense.category)),
+  // ]);
 
   const visibleExpenses = selectedCategory
     ? expenses.filter((expense) => expense.category === selectedCategory)
@@ -25,20 +26,17 @@ function App() {
     setExpenses(expenses.filter((expense) => expense.id !== id));
   };
 
-  const handleSubmit = (title: string, amount: number, category: string) => {
-    const newID = expenses.length + 1;
-    setExpenses([
-      ...expenses,
-      { id: newID, title: title, amount: amount, category: category },
-    ]);
-  };
-
   return (
     <>
-      <ExpenseForm categories={categories} onSubmit={handleSubmit} />
+      <div className="mb-5">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
       <ExpenseFilter
         onSelectCategory={(category) => setSelectedCategory(category)}
-        categories={categories}
       />
       <ExpenseList expenses={visibleExpenses} onDelete={handleDelete} />
     </>
--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,82 +1,93 @@
-import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import categories from "../categories";
 
 const schema = z.object({
   title: z
     .string()
-    .min(3, { message: "Title should be at least 3 characters." }),
+    .min(3, { message: "Title should be at least 3 characters." })
+    .max(50),
   amount: z
     .number({ invalid_type_error: "Amount is required" })
-    .positive({ message: "Amount must be greater than 0" }),
-  category: z.string().min(3, { message: "Category is required" }),
+    .min(0.01)
+    .max(100_000),
+  category: z.enum(categories, {
+    errorMap: () => ({ message: "Category is required" }),
+  }),
 });
 
-type FormData = z.infer<typeof schema>;
+type ExpenseFormData = z.infer<typeof schema>;
 
 interface Props {
-  categories: string[];
-  onSubmit: (title: string, amount: number, category: string) => void;
+  onSubmit: (data: ExpenseFormData) => void;
 }
 
-const ExpenseForm = ({ categories, onSubmit }: Props) => {
+const ExpenseForm = ({ onSubmit }: Props) => {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors, isValid },
-  } = useForm<FormData>({ resolver: zodResolver(schema) });
+  } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
 
   return (
     <form
-      action=""
-      className="mb3"
-      onSubmit={handleSubmit((data) =>
-        onSubmit(data.title, data.amount, data.category)
-      )}
+      onSubmit={handleSubmit((data) => {
+        onSubmit(data);
+        reset();
+      })}
     >
-      <label htmlFor="title" className="form-label">
-        Title
-      </label>
-      <input
-        {...register("title")}
-        id="title"
-        type="text"
-        className="form-control mb-3"
-      />
-      {errors.title && <p className="text-danger">{errors.title.message}</p>}
+      <div className="mb3">
+        <label htmlFor="title" className="form-label">
+          Title
+        </label>
+        <input
+          {...register("title")}
+          id="title"
+          type="text"
+          className="form-control"
+        />
+        {errors.title && <p className="text-danger">{errors.title.message}</p>}
+      </div>
 
-      <label htmlFor="amount" className="form-label">
-        Amount
-      </label>
-      <input
-        {...register("amount", { valueAsNumber: true })}
-        id="amount"
-        type="number"
-        className="form-control mb-3"
-      />
-      {errors.amount && <p className="text-danger">{errors.amount.message}</p>}
+      <div className="mb3">
+        <label htmlFor="amount" className="form-label">
+          Amount
+        </label>
+        <input
+          {...register("amount", { valueAsNumber: true })}
+          id="amount"
+          type="number"
+          className="form-control"
+        />
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
+      </div>
 
-      <label htmlFor="category" className="form-label">
-        Category
-      </label>
-      <select
-        {...register("category")}
-        name="category"
-        id="category"
-        className="form-select"
-        defaultValue=""
-      >
-        <option></option>
-        {categories.map((category) => (
-          <option key={category}>{category}</option>
-        ))}
-      </select>
-      {errors.category && (
-        <p className="text-danger">{errors.category.message}</p>
-      )}
+      <div className="mb3">
+        <label htmlFor="category" className="form-label">
+          Category
+        </label>
+        <select
+          {...register("category")}
+          name="category"
+          id="category"
+          className="form-select"
+        >
+          <option value=""></option>
+          {categories.map((category) => (
+            <option key={category}>{category}</option>
+          ))}
+        </select>
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
+      </div>
 
       <button
-        disabled={!isValid}
+        // disabled={!isValid}
         type="submit"
         className="btn btn-primary mt-3"
       >
